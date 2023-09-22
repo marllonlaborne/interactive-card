5c7f32c04cbc27cb6fa260a1052d33a23e8c1350
Marllon <99225101+marllonlaborne@users.noreply.github.com>
2023-09-22 19:40:43 -0300

    update function to improve card number validation

--- js/app.js
+++ js/app.js
@@ -31,26 +31,42 @@ const validateFields = () => {
 }
 
 const validateCardNumber = (cardNumber) => {
-  cardNumber = cardNumber.replace(/\s/g, '').replace('/[^0-9]/g', '')
+  let valid = true
+  const cardNumberInput = form.querySelector('.card-number')
+  cardNumber = cardNumber.replace(/\s/g, '').replace(/[^0-9]/g, '')
 
-  if (cardNumber.length < 13 || cardNumber.length > 16) return false
-  if (typeof cardNumber.value !== 'number') return false
+  if (cardNumber.length < 13 || cardNumber.length > 16) {
+    addErrorMessage(cardNumberInput, 'Card number must be between 13 and 16 digits')
+    valid = false
+  }
+
+  if (!/^[0-9]+$/.test(cardNumber)) {
+    addErrorMessage(cardNumberInput, 'Wrong format, numbers only')
+    valid = false
+  }
 
   const isDoubleDigit = (index) => index % 2 === 1
-  const sum = cardNumber.reduce((accumulator, digit, index) => {
-    let number = parseInt(digit)
+  const digits = cardNumber.split('').map(Number)
+  const sum = digits.reduce((accumulator, digit, index) => {
+    let number = digit
 
     if (isDoubleDigit(index)) {
       number *= 2
       if (number > 9) {
-        num -= 9
+        number -= 9
       }
     }
 
     return accumulator + number
   }, 0)
 
-  return sum % 10 === 0
+  if (sum % 10 !== 0) {
+    addErrorMessage(cardNumberInput, 'Invalid card number')
+    cardNumberInput.style.borderColor = 'var(--input-errors)'
+    valid = false
+  }
+
+  return valid
 }
 
 const addErrorMessage = (field, message) => {
